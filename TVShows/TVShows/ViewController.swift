94a92e1f1846b4b8bda0825ccc1bdce00649690e
Soksijada <k.bakovic7@gmail.com>
2019-07-06 14:17:20 +0200

    This is example commit.

--- TVShows/TVShows/ViewController.swift
+++ TVShows/TVShows/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myswitch: UISwitch!
+    @IBOutlet weak var labela: UILabel!
+    @IBOutlet weak var firstNumberOutlet: UITextField!
+    @IBOutlet weak var secondNumberOutlet: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var sumLabel: UILabel!
+    var brojPritisaka = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myswitch.setOn(false, animated: true)
+        configureTextFields()
     }
-
-
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        let color: UIColor = sender.isOn ? .yellow : .white
+        view.backgroundColor = color
+        brojPritisaka += 1
+        labela.text = "Broj pritisaka: \(brojPritisaka)"
+    }
+    
+    
+    func configureTextFields() {
+        firstNumberOutlet.delegate = self
+        secondNumberOutlet.delegate = self
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if firstNumberOutlet.text != "" && secondNumberOutlet.text != "" {
+            let a = Int(firstNumberOutlet.text!)
+            let b = Int(secondNumberOutlet.text!)
+            let rez = a! + b!
+            sumLabel.text = "Sum: \(rez)"
+        }
+                }
 }
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
 
